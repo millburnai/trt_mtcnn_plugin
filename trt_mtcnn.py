02391e9fc431fba78486c444f33eaac851dcfab7
Ryan Park <22parkr@millburn.org>
2021-02-16 14:10:48 -0500

    Update trt mtcnn interface

--- trt_mtcnn.py
+++ trt_mtcnn.py
@@ -10,6 +10,35 @@ PIXEL_MEAN = 127.5
 PIXEL_SCALE = 0.0078125
 
 
+class TrtMTCNNWrapper:
+
+    LANDMARKS = ["left_eye", "mouth_left", "nose", "right_eye", "mouth_right"]
+
+    def __init__(self, pnet, rnet, onet):
+        self.mtcnn = TrtMtcnn(pnet, rnet, onet)
+
+    def detect_faces(self, img, minsize=40):
+        result = []
+
+        boxes, landmarks = self.mtcnn.detect(img[:, :, ::-1], minsize=minsize)
+        for box, features in zip(boxes, landmarks):
+            face = {}
+
+            face["confidence"] = box[-1]
+
+            x1, y1, x2, y2 = [int(coord) for coord in box[:-1]]
+            face["box"] = [x1, y1, x2 - x1, y2 - y1]
+
+            x_key, y_key = features[:5], features[5:]
+            face["keypoints"] = {
+                feat: (int(x), int(y)) for feat, x, y in zip(self.LANDMARKS, x_key, y_key)
+            }
+
+            result.append(face)
+
+        return result
+
+
 def convert_to_1x1(boxes):
     """Convert detection boxes to 1:1 sizes
 
