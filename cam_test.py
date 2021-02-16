d5b47d0c62978885e29fe4df0e0648a0ed32eb45
Ryan Park <22parkr@millburn.org>
2021-02-16 14:28:25 -0500

    Add working livestream mtcnn test

---
+++ cam_test.py
@@ -0,0 +1,36 @@
+import sys
+from timeit import default_timer as timer
+
+import cv2
+
+sys.path.insert(1, "../..")
+from trt_mtcnn import TrtMTCNNWrapper
+from utils.visuals import GraphicsRenderer, Camera
+
+
+if __name__ == "__main__":
+    mtcnn = TrtMTCNNWrapper("mtcnn/det1.engine",
+                            "mtcnn/det2.engine",
+                            "mtcnn/det3.engine")
+
+    cap = Camera(threaded=False)
+    graphics = GraphicsRenderer()
+
+    while True:
+        _, frame = cap.read()
+
+        try:
+            start = timer()
+            result = mtcnn.detect_faces(frame)
+            elapsed = timer() - start
+
+            for person in result:
+                graphics.add_graphics(frame, person, True, "", elapsed)
+            print(f"{elapsed * 1000} ms")
+
+        except IndexError:
+            print("no face detected")
+
+        cv2.imshow("cam test", frame)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
